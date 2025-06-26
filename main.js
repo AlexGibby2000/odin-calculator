f6d1fbf374c3f508cf789118c7d2ffe0c2c2ecd4
AlexGibby2000 <42209825+AlexGibby2000@users.noreply.github.com>
2025-06-25 20:08:20 -0400

    added keyboard support

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded",function(){
     numbers.forEach((number)=>number.addEventListener("click",function(e){
         handleNumber(e.target.textContent);
         currentScreen.textContent=currentValue;
-    }))
+    }));
 
     operators.forEach((op)=>op.addEventListener("click",function(e){
         if(operator===''){
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded",function(){
         previousScreen.textContent = previousValue + " " + operator;
         currentScreen.textContent=currentValue;
         }
-    }))
+    }));
 
     clear.addEventListener("click", function(){
         previousValue = '';
@@ -32,13 +32,13 @@ document.addEventListener("DOMContentLoaded",function(){
         operator = '';
         previousScreen.textContent= currentValue;
         currentScreen.textContent= currentValue;
-    })
+    });
     del.addEventListener("click",function(){
         if(currentValue.length>0){
             currentValue=currentValue.slice(0,-1);
         }
         currentScreen.textContent=currentValue;
-    })
+    });
 
     equal.addEventListener("click",function(){
         if(currentValue !='' && previousValue !=''){
@@ -55,12 +55,111 @@ document.addEventListener("DOMContentLoaded",function(){
                 }
                 }
         }
-    })
+    });
 
     decimal.addEventListener("click",function(){
         addDecimal();
-    })
-})
+    });
+
+    window.addEventListener("keydown",function(e){
+        switch(e.key){
+            case "0":
+                handleNumber("0");
+                currentScreen.textContent=currentValue;
+                break;
+            case "1":
+                handleNumber("1");
+                currentScreen.textContent=currentValue;
+                break;
+            case "2":
+                handleNumber("2");
+                currentScreen.textContent=currentValue;
+                break;
+            case "3":
+                handleNumber("3");
+                currentScreen.textContent=currentValue;
+                break;    
+            case "4":
+                handleNumber("4");
+                currentScreen.textContent=currentValue;
+                break;
+            case "5":
+                handleNumber("5");
+                currentScreen.textContent=currentValue;
+                break;
+            case "6":
+                handleNumber("6");
+                currentScreen.textContent=currentValue;
+                break;
+            case "7":
+                handleNumber("7");
+                currentScreen.textContent=currentValue;
+                break;
+            case "8":
+                handleNumber("8");
+                currentScreen.textContent=currentValue;
+                break;
+            case "9":
+                handleNumber("9");
+                currentScreen.textContent=currentValue;
+                break;
+            case "/":
+                if(operator===''){
+                    handleOperator('/');
+                    previousScreen.textContent = previousValue + " " + operator;
+                    currentScreen.textContent=currentValue;
+                    }
+                break;
+            case "*":
+            case "x":
+                if(operator===''){
+                    handleOperator('x');
+                    previousScreen.textContent = previousValue + " " + operator;
+                    currentScreen.textContent=currentValue;
+                    }
+                break;
+            case "-":
+                if(operator===''){
+                    handleOperator('-');
+                    previousScreen.textContent = previousValue + " " + operator;
+                    currentScreen.textContent=currentValue;
+                    }
+                break;
+            case "+":
+                if(operator===''){
+                    handleOperator('+');
+                    previousScreen.textContent = previousValue + " " + operator;
+                    currentScreen.textContent=currentValue;
+                    }
+                break;
+            case ".":
+                addDecimal();
+                break;
+            case "Enter":
+            case "=":
+                if(currentValue !='' && previousValue !=''){
+                    calculate();
+                    previousScreen.textContent='';
+                    if(typeof previousValue==="string"){
+                        currentScreen.textContent=previousValue;
+                    }
+                    else{
+                        if(previousValue.length<=18){
+                            currentScreen.textContent = previousValue;
+                        } else{
+                            currentScreen.textContent =(Number(previousValue).toExponential(8)).toString();
+                        }
+                        }
+                }
+                break;
+            case "Backspace":
+                if(currentValue.length>0){
+                    currentValue=currentValue.slice(0,-1);
+                }
+                currentScreen.textContent=currentValue;
+        }
+    });
+});
 
 function handleNumber(num){
     if(currentValue.length<=16){
